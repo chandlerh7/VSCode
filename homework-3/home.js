765adb5abf0377b908a95ff9934809f0764f6bcb
chandler7 <chandlerrh7@gmail.com>
2025-01-31 01:47:31 -0500

    added const to the home.js file

---
+++ homework-3/home.js
@@ -0,0 +1,10 @@
+const Radius ="5";
+console.log("The radius is", Radius);
+
+const Area ="Pl * r**2";
+console.log("The area is", Area);
+
+const Perimeter = "2 * Pl * r";
+console.log("The perimeter is", Perimeter)
+
+console.log("I decided to chose the declaration word 'const' because inregards to the prompt, these values are not supposed to be changed in any way and I know that const is used when something cannot be reassigned or redeclared.");
