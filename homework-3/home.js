b882dc1557959392a77eba60ecf4c324f3cee69d
chandler7 <chandlerrh7@gmail.com>
2025-02-04 21:33:50 -0500

    Deleted an unnecessary space

--- homework-3/home.js
+++ homework-3/home.js
@@ -3,6 +3,6 @@ let Area = "A";
 let Perimeter = "P";
 let R = 5;
 let A = 3.14 * R**2;
-console.log(" The area is", A.toFixed(1));
+console.log("The area is", A.toFixed(1));
 let P = 2 * 3.14 * R;
 console.log("The perimeter is", P.toFixed(1));
