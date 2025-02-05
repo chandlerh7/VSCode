3d86f2891d68e85c4f5326488665f71698fc3bba
chandler7 <chandlerrh7@gmail.com>
2025-02-04 20:24:04 -0500

    added code to the console

--- homework-3/store.js
+++ homework-3/store.js
@@ -1,1 +1,13 @@
 let itemPrice = "P";
+console.log("The variable for item price is", itemPrice);
+
+let totalafterTax = "T";
+console.log("The variable for the total after tax is", totalafterTax);
+
+let discounttotalafterTax = "D";
+console.log("The variable for the discounted total after tax is", discounttotalafterTax);
+
+let P = 40.99;
+const T = P * (1 + 0.07);
+console.log("The toal after tax is", T.toFixed(2));
+
