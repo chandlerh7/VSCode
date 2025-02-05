f22b47bbde738e44b669207582235138198448a4
chandler7 <chandlerrh7@gmail.com>
2025-02-04 21:32:53 -0500

    deleted previous code uploaded the console and added more code to the console

--- homework-3/store.js
+++ homework-3/store.js
@@ -1,11 +1,6 @@
 let itemPrice = "P";
-console.log("The variable for item price is", itemPrice);
-
 let totalafterTax = "T";
-console.log("The variable for the total after tax is", totalafterTax);
-
 let discounttotalafterTax = "D";
-console.log("The variable for the discounted total after tax is", discounttotalafterTax);
 
 let P = 40.99;
 let T = P * (1 + 0.07);
