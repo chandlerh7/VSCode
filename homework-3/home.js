f7a3f944631feec57c1a587e53aec06e714624e2
chandler7 <chandlerrh7@gmail.com>
2025-02-04 21:21:51 -0500

    added code to the console

--- homework-3/home.js
+++ homework-3/home.js
@@ -1,10 +1,8 @@
-const Radius ="5";
-console.log("The radius is", Radius);
-
-const Area ="Pl * r**2";
-console.log("The area is", Area);
-
-const Perimeter = "2 * Pl * r";
-console.log("The perimeter is", Perimeter)
-
-console.log("I decided to chose the declaration word 'const' because inregards to the prompt, these values are not supposed to be changed in any way and I know that const is used when something cannot be reassigned or redeclared.");
+let Radius = "R";
+let Area = "A";
+let Perimeter = "P";
+let R = 5;
+let A = 3.14 * R**2;
+console.log(" The area is", A.toFixed(1));
+let P = 2 * 3.14 * R;
+console.log("The perimeter is", P.toFixed(1));
