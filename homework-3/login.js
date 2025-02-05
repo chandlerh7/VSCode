94dd1e0399cfae025f50c10cfe191e5eb7d26466
chandler7 <chandlerrh7@gmail.com>
2025-02-04 22:14:53 -0500

    added a space

--- homework-3/login.js
+++ homework-3/login.js
@@ -3,6 +3,7 @@ const startwithSpace = "S";
 const eightormoreCharacters = "C";
 const containsEat = "E";
 ("Comment: I decided to use const because I did not have any intention to reassign or redeclare the variables.")
+
 console.error(startwithSpace, "(The password you created starts with a space)");
 console.error(containsEat, "(The password you created contains the word 'eat')");
 console.log(eightormoreCharacters, "(The password you created has eight or more characters)");
