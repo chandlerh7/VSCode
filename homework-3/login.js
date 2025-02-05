dc860435779a95a7f4d9fca230be1207687bbda5
chandler7 <chandlerrh7@gmail.com>
2025-02-04 19:50:42 -0500

    added code to the pages

---
+++ homework-3/login.js
@@ -0,0 +1,15 @@
+let passwordText = "PT";
+console.log("The varaible for the password's text is", passwordText);
+
+let startwithSpace = "S";
+console.log("The variable for a password starting with a space is", startwithSpace);
+
+let eightormoreCharacters = "C";
+console.log("The variable for a password having eight or more characters is", eightormoreCharacters);
+
+let containsEat = "E";
+console.log("The variable for the password containing the word 'eat' is", containsEat);
+
+console.error(startwithSpace, "(The password you created starts with a space)");
+console.error(containsEat,"(The password you created contains the word 'eat')");
+console.log(eightormoreCharacters, "(The password you created has eight or more characters)")
