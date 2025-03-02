c2ce2441c9ba4a8b9a34e207519be16de36171ce
chandler7 <chandlerrh7@gmail.com>
2025-03-01 22:59:26 -0500

    added code to the html and created a js folder andd file

--- homework-4/lessons.js
+++ homework-4/lessons.js
@@ -13,9 +13,9 @@ console.log("ul textContent", ulRef.textContent);
 
 for (let i=0; i < listofInstruments.length; i++) {console.log("For Loop:", listofInstruments[i])}
 
-ulRef.innerHTML = "<li ='instruments'> Piano </li>";
-ulRef.innerHTML = "<li ='instruments'> Guitar </li>";
-ulRef.innerHTML = "<li ='instruments'> Violin </li>";
-ulRef.innerHTML = "<li ='instruments'> Drums </li>";
-ulRef.innerHTML = "<li ='instruments'> Saxophone </li>";
-ulRef.innerHTML = "<li ='instruments'> Flute </li>";
+//ulRef.innerHTML += "<li ='instruments'> Piano </li>";
+// ulRef.innerHTML += "<li ='instruments'> Guitar </li>";
+// ulRef.innerHTML += "<li ='instruments'> Violin </li>";
+// ulRef.innerHTML += "<li ='instruments'> Drums </li>";
+// ulRef.innerHTML += "<li ='instruments'> Saxophone </li>";
+// ulRef.innerHTML += "<li ='instruments'> Flute </li>";
