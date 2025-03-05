e4f7f3545bf1622ed0d483b4f2d40f7a09eeb8fd
chandler7 <chandlerrh7@gmail.com>
2025-03-05 16:49:31 -0500

    adding week 8 codes

---
+++ week-8/js/objects.js
@@ -0,0 +1,31 @@
+//Reference to DOM
+const detailSectionRef = document.querySelector("#details");
+
+//Data
+const pokemon = {
+    name: "Ponyta",
+    type: "Fire",
+    weakness: ["Water", "Ground", "Rock"],
+};
+
+const myParty = [
+    {name:"Ponyta", type:"Fire"},
+    {name:"Chimchar", type:"Fire"},
+    {name:"Meowth", type:"Normal"},
+    {name:"Houndoom", type:"Dark|Fire"},
+];
+
+console.log("My starter pokemon:", pokemon);
+console.log("My pokemon type:", pokemon.type);
+
+console.log("First pokemon in party",myParty[0]);
+console.log("Last pokemon's name", myParty[myParty.length - 1].name);
+
+for(let i= 0; i < myParty.length; i++) {
+    const currentPokemon = myParty[i];
+    detailsSectionRef.innerHTML += "<div>";
+    detailsSectionRef.innerHTML += "<h4>" + currentPokemon.name + "</h4>";
+    detailsSectionRef.innerHTML += "<p>Type(s):" + currentPokemon.type + "</p>";
+    detailsSectionRef.innerHTML += "</div>";
+}
+
