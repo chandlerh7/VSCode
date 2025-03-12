101d99508b5f51cef6d032fee1a79a6ad4731720
chandler7 <chandlerrh7@gmail.com>
2025-03-12 15:41:50 -0400

    added live links and group events to readme.md and added code to both the html and js

---
+++ midterm/readme.md
@@ -0,0 +1,2 @@
+## Group Events
+### Live Links 
---
+++ midterm/restaurant.js
@@ -0,0 +1,13 @@
+const menuItemsUlRef = document.getElementById("menu");
+const cartTableUlRef = document.getElementById("cart");
+const noItemsUlRef = document.getElementById("no-items");
+
+menuItems = ["Hamburger - $2.99",
+    "Cheeseburger - $3.99",
+    "Cheese Pizza - $3.99",
+    "Pepperoni Pizza - $4.99",
+    "Sausage Pizza - $4.99",
+    "Caesar Salad - $3.99",
+    "Fries - $1.99",
+    "Onion Rings - $2.49"]
+cart = []
