c30cf09b9c1dfd69e21792264c7aa7f3bfc5e762
chandler7 <chandlerrh7@gmail.com>
2025-03-12 16:35:02 -0400

    Added a css to the html. Added functions and for loops to the js. Changed the background-color on the css.

--- midterm/restaurant.js
+++ midterm/restaurant.js
@@ -1,5 +1,5 @@
 const menuItemsUlRef = document.getElementById("menu");
-const cartTableUlRef = document.getElementById("cart");
+let cartTableUlRef = document.getElementById("cart");
 const noItemsUlRef = document.getElementById("no-items");
 
  const menuItems = ["Hamburger - $2.99",
@@ -16,9 +16,35 @@ const cart = [];
 //add the logic to show the no-items row if there are no items in the cart list//
 
 for (let i = 0; i < menuItems.length; i++){
-    console.log(menuItems[i]);
+    const newLi = document.createElement("li");
+    newLi.innerText = menuItems[i];
+
+    const newBtn = document.createElement("button");
+    newBtn.innerText = "+";
+    newBtn.dataset.item = menuItems[i];
+    newLi.appendChild(newBtn);
+    newBtn.onclick = addItemToCart;
+    menuItemsUlRef.appendChild(newLi); 
 }
 
 for (let i = 0; i < cart.length; i++){
     console.log(cart[i]);
 }
+
+ function addItemToCart(e) {
+    cart.push(e.target.dataset.item);
+    showCart();
+ }
+
+ function showCart() {
+    cartTableUlRef=""
+    if (cart.length === 0) {
+        noItemsUlRef.style.display = "block";
+      } else {
+        noItemsUlRef.style.display = "none";
+      }
+
+      for (let i = 0; i < cart.length; i++) {
+        cartTableUlRef.innerHTML += "<li>" + cart[i] + "</li>";
+      }
+ }
